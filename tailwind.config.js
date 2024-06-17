eb023735a97def6e5d79a2652c10b37b8b6e31e8
Marek Zelinka <mzelinka17@gmail.com>
2024-06-17 10:37:42 +0200

    fix inter font name

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         gray: colors.slate,
